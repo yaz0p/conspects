7dcf5c15837da25b0cdbec93a9885db01f07fdd3
yazop <vladpopov9@mail.ru>
2024-07-12 17:45:41 +0300

    c conspect

---
+++ c/datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main()
+{
+    int age = 40; // integer - целые числа в c
+    double gpa = 3.5; // числа с точкой (дробные)
+    char grade = 'A'; // буква/знак
+    char phrase[] = "FooBar"; // массив из букв (строка)
+    printf("Age %d, gpa %f, grade %c, phrase %s", age, gpa, grade, phrase); // d - int, f - float, c - char, s - char array
+}
