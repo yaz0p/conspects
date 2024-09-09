b5d24ca3a62a6be03d535013eefbe8ca152bdfc0
Владислав Попов <vladislavpopov@MacBook-Air-Vladislav.local>
2024-09-09 22:39:01 +0300

    generics

---
+++ python/generic_type.py
@@ -0,0 +1,28 @@
+# Дженерики -- реализация параметрического полиморфизма. 
+# В случае с питоном ( типы определяются объектами во время рантайма )
+# помогает отслеживать возможные ошибки в написании кода. 
+from typing import TypeVar, TypeAlias
+
+T = TypeVar('T')
+
+def foo(bar: list[T]) -> T:
+     return bar[0]
+
+a = foo([1,2,3])
+b = foo(['a','b','c'])
+
+print(a + 2)
+print(b + 'b')
+
+# Так же в модуле typing присутствует TypeAlias, позволяющий алиасить кастомные типы
+
+Point: TypeAlias = tuple[float, float]
+Triangle: TypeAlias = tuple[Point, Point, Point]
+
+print(type(Point))
+print(type(Triangle))
+print(type(T))
+point: Point = (1.2, 1.3) # кастомный класс, позволяющий отслеживать состояние типов
+triangle: Triangle = ((1.2, 1.3), (1.2, 1.3), (1.2, 1.3))
+
+triangles: Triangle = ((1.2, 1.3, 4), (1.2, 1.3), (1.2, 1.3)) # mypy подсветит ошибку
